064c03aa4ae433fdc07766ff11583f7632848345
Dan Ward <dan.j.ward@gmail.com>
2014-05-29 11:43:05 +0100

    Nuget

--- Extensions/Properties/AssemblyInfo.cs
+++ Extensions/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("ZeroTwoTwelve.Extensions")]
-[assembly: AssemblyDescription("Miscelaneous Extensions and Utilities")]
+[assembly: AssemblyDescription("Miscellaneous Extensions and Utilities")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Dan Ward")]
 [assembly: AssemblyProduct("Extensions")]
 [assembly: AssemblyCopyright("Copyright ©  2012-14")]
 [assembly: AssemblyTrademark("MIT License")]
