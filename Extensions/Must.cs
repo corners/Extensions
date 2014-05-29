67a617518e07469d1e647ce764c086eecbe8f82f
Dan Ward <dan.j.ward@gmail.com>
2014-05-29 12:06:12 +0100

    Added range check

--- Extensions/Must.cs
+++ Extensions/Must.cs
@@ -13,5 +13,13 @@ namespace ZeroTwoTwelve.Extensions
                 throw new ArgumentNullException(name);
             return value;
         }
+
+        public static void BeInRange(int value, int min, int length, string name = "")
+        {
+            int max = min + length;
+            if (value < min || value > max)
+                throw new ArgumentOutOfRangeException(name, string.Format("Must be in range {0} to {1}", min, max));
+        }
+
     }
 }
